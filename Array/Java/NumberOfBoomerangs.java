6059440ebf2174d0c9e5c56f6891a54f81d7a1d1
Alex Jiang <pigflyjky@gmail.com>
2019-07-08 15:18:47 +1000

    [Algorithm](Array): add problem set for number of boomerangs

---
+++ Array/Java/NumberOfBoomerangs.java
@@ -0,0 +1,29 @@
+/**
+ * This code snippet is copyright at qmz.com.au
+ * - qmz.com.au is a top-ranking IT consulting firm that specialised in
+ * - Mobile Development, Web Development, Machine Learning, and Cloud Computing.
+ * - We provide individual and company training for professional development.
+ * - Find out more information in our official site：
+ * - https://qmz.com.au
+ */
+
+// Given n points in the plane that are all pairwise distinct, a "boomerang" is a tuple of points (i, j, k) such that the distance between i and j equals the distance between i and k (the order of the tuple matters).
+
+// Find the number of boomerangs. You may assume that n will be at most 500 and coordinates of points are all in the range [-10000, 10000] (inclusive).
+
+// Example:
+
+// Input:
+// [[0,0],[1,0],[2,0]]
+
+// Output:
+// 2
+
+// Explanation:
+// The two boomerangs are [[1,0],[0,0],[2,0]] and [[1,0],[2,0],[0,0]]
+
+class Solution {
+    public int numberOfBoomerangs(int[][] points) {
+        
+    }
+}
