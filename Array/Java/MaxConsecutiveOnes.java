0cb4af2e4684e7745d6c2b4685f096eedd07d0b0
Alex Jiang <pigflyjky@gmail.com>
2019-07-08 14:55:28 +1000

    [Algorithm](Array): add problem set for max consecutive ones

---
+++ Array/Java/MaxConsecutiveOnes.java
@@ -0,0 +1,26 @@
+/**
+ * This code snippet is copyright at qmz.com.au
+ * - qmz.com.au is a top-ranking IT consulting firm that specialised in
+ * - Mobile Development, Web Development, Machine Learning, and Cloud Computing.
+ * - We provide individual and company training for professional development.
+ * - Find out more information in our official site：
+ * - https://qmz.com.au
+ */
+
+// Given a binary array, find the maximum number of consecutive 1s in this array.
+
+// Example 1:
+// Input: [1,1,0,1,1,1]
+// Output: 3
+// Explanation: The first two digits or the last three digits are consecutive 1s.
+//     The maximum number of consecutive 1s is 3.
+// Note:
+
+// The input array will only contain 0 and 1.
+// The length of input array is a positive integer and will not exceed 10,000
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        
+    }
+}
