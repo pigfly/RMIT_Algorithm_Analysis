0643d94ab5605723b7444dac106f71d0be13ccd9
Alex Jiang <pigflyjky@gmail.com>
2019-07-10 15:09:55 +1000

    [Algorithm](Array): problem majority element II import

---
+++ Array/Java/MajorityElementII.java
@@ -0,0 +1,27 @@
+/**
+ * This code snippet is copyright at qmz.com.au
+ * - qmz.com.au is a top-ranking IT consulting firm that specialised in
+ * - Mobile Development, Web Development, Machine Learning, and Cloud Computing.
+ * - We provide individual and company training for professional development.
+ * - Find out more information in our official site：
+ * - https://qmz.com.au
+ */
+
+// Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
+
+// Note: The algorithm should run in linear time and in O(1) space.
+
+// Example 1:
+
+// Input: [3,2,3]
+// Output: [3]
+// Example 2:
+
+// Input: [1,1,1,3,3,2,2,2]
+// Output: [1,2]
+
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        
+    }
+}
