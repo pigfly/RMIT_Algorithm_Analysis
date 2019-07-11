b4d3b0eeb05ad142ec76a2c6040fbb8fa205f782
Alex Jiang <pigflyjky@gmail.com>
2019-07-11 14:10:56 +1000

    [Project](code): update copyright logo

--- Array/Java/ValidTicTacToeState.java
+++ Array/Java/ValidTicTacToeState.java
@@ -1,10 +1,18 @@
-/**
+/**                                                                                 
+  ,dW"Yvd `7MMpMMMb.pMMMb.  M"""MMV     ,p6"bo   ,pW"Wq.`7MMpMMMb.pMMMb.      ,6"Yb.`7MM  `7MM  
+ ,W'   MM   MM    MM    MM  '  AMV     6M'  OO  6W'   `Wb MM    MM    MM     8)   MM  MM    MM  
+ 8M    MM   MM    MM    MM    AMV      8M       8M     M8 MM    MM    MM      ,pm9MM  MM    MM  
+ YA.   MM   MM    MM    MM   AMV  , ,, YM.    , YA.   ,A9 MM    MM    MM  ,, 8M   MM  MM    MM  
+  `MbmdMM .JMML  JMML  JMML.AMMmmmM db  YMbmd'   `Ybmd9'.JMML  JMML  JMML.db `Moo9^Yo.`Mbod"YML.
+       MM                                                                                       
+     .JMML.                                                                                     
+       
  * This code snippet is copyright at qmz.com.au
  * - qmz.com.au is a top-ranking IT consulting firm that specialised in
  * - Mobile Development, Web Development, Machine Learning, and Cloud Computing.
  * - We provide individual and company training for professional development.
  * - Find out more information in our official site：
- * - https://qmz.com.au
+ * - https://qmz.com.au                                                                   
  */
 
 // A Tic-Tac-Toe board is given as a string array board. Return True if and only if it is possible to reach this board position during the course of a valid tic-tac-toe game.
