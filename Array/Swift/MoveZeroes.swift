0025a4cde6a155981d4341757ec21145becc0aad
Alex Jiang <pigflyjky@gmail.com>
2019-07-14 18:51:12 +1000

    [Algorithm](Array): problem move zeroes import

---
+++ Array/Swift/MoveZeroes.swift
@@ -0,0 +1,34 @@
+/**                                                                                 
+  ,dW"Yvd `7MMpMMMb.pMMMb.  M"""MMV     ,p6"bo   ,pW"Wq.`7MMpMMMb.pMMMb.      ,6"Yb.`7MM  `7MM  
+ ,W'   MM   MM    MM    MM  '  AMV     6M'  OO  6W'   `Wb MM    MM    MM     8)   MM  MM    MM  
+ 8M    MM   MM    MM    MM    AMV      8M       8M     M8 MM    MM    MM      ,pm9MM  MM    MM  
+ YA.   MM   MM    MM    MM   AMV  , ,, YM.    , YA.   ,A9 MM    MM    MM  ,, 8M   MM  MM    MM  
+  `MbmdMM .JMML  JMML  JMML.AMMmmmM db  YMbmd'   `Ybmd9'.JMML  JMML  JMML.db `Moo9^Yo.`Mbod"YML.
+       MM                                                                                       
+     .JMML.                                                                                     
+       
+ * This code snippet is copyright at qmz.com.au
+ * - qmz.com.au is a top-ranking IT consulting firm that specialised in
+ * - Mobile Development, Web Development, Machine Learning, and Cloud Computing.
+ * - We provide individual and company training for professional development.
+ * - Find out more information in our official site：
+ * - https://qmz.com.au                                                                   
+ */
+
+// Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Example:
+
+// Input: [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Note:
+
+// You must do this in-place without making a copy of the array.
+// Minimize the total number of operations.
+
+
+class Solution {
+    func moveZeroes(_ nums: inout [Int]) {
+        
+    }
+}
