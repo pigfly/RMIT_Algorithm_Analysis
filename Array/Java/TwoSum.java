6103605630d7befef3d55dd0251c23ead228bbf6
Alex Jiang <pigflyjky@gmail.com>
2019-07-14 19:07:04 +1000

    [Algorithm](Array): problem two sum import

---
+++ Array/Java/TwoSum.java
@@ -0,0 +1,34 @@
+/**                                                                                 
+  ,dW"Yvd `7MMpMMMb.pMMMb.  M"""MMV     ,p6"bo   ,pW"Wq.`7MMpMMMb.pMMMb.      ,6"Yb.`7MM  `7MM  
+ ,W'   MM   MM    MM    MM  '  AMV     6M'  OO  6W'   `Wb MM    MM    MM     8)   MM  MM    MM  
+ 8M    MM   MM    MM    MM    AMV      8M       8M     M8 MM    MM    MM      ,pm9MM  MM    MM  
+ YA.   MM   MM    MM    MM   AMV  , ,, YM.    , YA.   ,A9 MM    MM    MM  ,, 8M   MM  MM    MM  
+  `MbmdMM .JMML  JMML  JMML.AMMmmmM db  YMbmd'   `Ybmd9'.JMML  JMML  JMML.db `Moo9^Yo.`Mbod"YML.
+       MM                                                                                       
+     .JMML.                                                                                     
+       
+ * This code snippet is copyright at qmz.com.au
+ * - qmz.com.au is a top-ranking IT consulting firm that specialised in
+ * - Mobile Development, Web Development, Machine Learning, and Cloud Computing.
+ * - We provide individual and company training for professional development.
+ * - Find out more information in our official site：
+ * - https://qmz.com.au                                                                   
+ */
+
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// Example:
+
+// Given nums = [2, 7, 11, 15], target = 9,
+
+// Because nums[0] + nums[1] = 2 + 7 = 9,
+// return [0, 1].
+
+
+class Solution {
+  public int[] twoSum(int[] nums, int target) {
+      
+  }
+}
