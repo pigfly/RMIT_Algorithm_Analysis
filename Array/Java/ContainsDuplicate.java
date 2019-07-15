802a3ea41d2098e621847392d0352bf00a6379bd
Alex Jiang <pigflyjky@gmail.com>
2019-07-15 16:06:15 +1000

    [Algorithm](Array)✅: solution for contain duplicate

--- Array/Java/ContainsDuplicate.java
+++ Array/Java/ContainsDuplicate.java
@@ -1,18 +1,17 @@
-/**                                                                                 
-  ,dW"Yvd `7MMpMMMb.pMMMb.  M"""MMV     ,p6"bo   ,pW"Wq.`7MMpMMMb.pMMMb.      ,6"Yb.`7MM  `7MM  
- ,W'   MM   MM    MM    MM  '  AMV     6M'  OO  6W'   `Wb MM    MM    MM     8)   MM  MM    MM  
- 8M    MM   MM    MM    MM    AMV      8M       8M     M8 MM    MM    MM      ,pm9MM  MM    MM  
- YA.   MM   MM    MM    MM   AMV  , ,, YM.    , YA.   ,A9 MM    MM    MM  ,, 8M   MM  MM    MM  
-  `MbmdMM .JMML  JMML  JMML.AMMmmmM db  YMbmd'   `Ybmd9'.JMML  JMML  JMML.db `Moo9^Yo.`Mbod"YML.
-       MM                                                                                       
-     .JMML.                                                                                     
-       
- * This code snippet is copyright at qmz.com.au
- * - qmz.com.au is a top-ranking IT consulting firm that specialised in
- * - Mobile Development, Web Development, Machine Learning, and Cloud Computing.
- * - We provide individual and company training for professional development.
- * - Find out more information in our official site：
- * - https://qmz.com.au                                                                   
+import java.util.HashMap;
+
+/**
+ * ,dW"Yvd `7MMpMMMb.pMMMb. M"""MMV ,p6"bo ,pW"Wq.`7MMpMMMb.pMMMb. ,6"Yb.`7MM
+ * `7MM ,W' MM MM MM MM ' AMV 6M' OO 6W' `Wb MM MM MM 8) MM MM MM 8M MM MM MM MM
+ * AMV 8M 8M M8 MM MM MM ,pm9MM MM MM YA. MM MM MM MM AMV , ,, YM. , YA. ,A9 MM
+ * MM MM ,, 8M MM MM MM `MbmdMM .JMML JMML JMML.AMMmmmM db YMbmd' `Ybmd9'.JMML
+ * JMML JMML.db `Moo9^Yo.`Mbod"YML. MM .JMML.
+ * 
+ * This code snippet is copyright at qmz.com.au - qmz.com.au is a top-ranking IT
+ * consulting firm that specialised in - Mobile Development, Web Development,
+ * Machine Learning, and Cloud Computing. - We provide individual and company
+ * training for professional development. - Find out more information in our
+ * official site： - https://qmz.com.au
  */
 
 // Given an array of integers, find if the array contains any duplicates.
@@ -35,6 +34,20 @@
 
 class Solution {
   public boolean containsDuplicate(int[] nums) {
-      
+    if (nums.length == 0 || nums.length == 1) { return false; }
+
+    HashMap<Integer, Integer> numToCount = new HashMap<>();
+    for (int num : nums) {
+      numToCount.put(num, numToCount.getOrDefault(num, 0) + 1);
+      if (numToCount.containsKey(num) && numToCount.get(num) > 1) {
+        return true;
+      } else if (numToCount.containsKey(num)) {
+        numToCount.put(num, numToCount.get(num) + 1);
+      } else {
+        numToCount.put(num, 0);
+      }
+    }
+    
+    return false;
   }
 }
