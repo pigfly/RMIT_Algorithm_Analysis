f4d56b26bcd1dfe8313be24709de09a6dcd060ad
Alex Jiang <pigflyjky@gmail.com>
2019-07-17 22:28:51 +1000

    [Algorithm](LinkedList): problem import valid parentheses

--- README.md
+++ README.md
@@ -134,7 +134,7 @@ It tries to port as many concepts from the original version as possible, but som
 ##### Stack
 | Title | Solution | Difficulty | Time | Space |
 | ----- | -------- | ---------- | ---- | ----- |
-[Valid Parentheses](https://leetcode.com/problems/valid-parentheses/)| [Swift](./Stack/ValidParentheses.swift)| Easy| O(n)| O(n)|
+[Valid Parentheses](./Stack/Java/ValidParentheses.java)| [Java](./Stack/Java/ValidParentheses.java), [Swift](./Stack/Swift/ValidParentheses.swift)| Easy| O(n)| O(n)|
 [Longest Valid Parentheses](https://leetcode.com/problems/longest-valid-parentheses/)| [Swift](./Stack/LongestValidParentheses.swift)| Hard| O(n)| O(n)|
 [Evaluate Reverse Polish Notation](https://leetcode.com/problems/evaluate-reverse-polish-notation/)| [Swift](./Stack/EvaluateReversePolishNotation.swift)| Medium| O(n)| O(n)|
 [Simplify Path](https://leetcode.com/problems/simplify-path/)| [Swift](./Stack/SimplifyPath.swift)| Medium| O(n)| O(n)|
---
+++ Stack/Java/ValidParentheses.java
@@ -0,0 +1,52 @@
+/**                                                                                 
+  ,dW"Yvd `7MMpMMMb.pMMMb.  M"""MMV     ,p6"bo   ,pW"Wq.`7MMpMMMb.pMMMb.      ,6"Yb.`7MM  `7MM  
+ ,W'   MM   MM    MM    MM  '  AMV     6M'  OO  6W'   `Wb MM    MM    MM     8)   MM  MM    MM  
+ 8M    MM   MM    MM    MM    AMV      8M       8M     M8 MM    MM    MM      ,pm9MM  MM    MM  
+ YA.   MM   MM    MM    MM   AMV  , ,, YM.    , YA.   ,A9 MM    MM    MM  ,, 8M   MM  MM    MM  
+  `MbmdMM .JMML  JMML  JMML.AMMmmmM db  YMbmd'   `Ybmd9'.JMML  JMML  JMML.db `Moo9^Yo.`Mbod"YML.
+       MM                                                                                       
+     .JMML.                                                                                     
+       
+ * This code snippet is copyright at qmz.com.au
+ * - qmz.com.au is a top-ranking IT consulting firm that specialised in
+ * - Mobile Development, Web Development, Machine Learning, and Cloud Computing.
+ * - We provide individual and company training for professional development.
+ * - Find out more information in our official site：
+ * - https://qmz.com.au                                                                   
+ */
+
+// Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Note that an empty string is also considered valid.
+
+// Example 1:
+
+// Input: "()"
+// Output: true
+// Example 2:
+
+// Input: "()[]{}"
+// Output: true
+// Example 3:
+
+// Input: "(]"
+// Output: false
+// Example 4:
+
+// Input: "([)]"
+// Output: false
+// Example 5:
+
+// Input: "{[]}"
+// Output: true
+
+class Solution {
+  public boolean isValid(String s) {
+    
+    return false;
+  }
+}
