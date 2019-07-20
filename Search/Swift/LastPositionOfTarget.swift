164faf358ab290e3dd593c2dbf34bf8947f31080
Alex Jiang <pigflyjky@gmail.com>
2019-07-20 23:24:15 +1000

    [Algorithm](Search): problem import last position of target

---
+++ Search/Swift/LastPositionOfTarget.swift
@@ -0,0 +1,37 @@
+/**                                                                                 
+  ,dW"Yvd `7MMpMMMb.pMMMb.  M"""MMV     ,p6"bo   ,pW"Wq.`7MMpMMMb.pMMMb.      ,6"Yb.`7MM  `7MM  
+ ,W'   MM   MM    MM    MM  '  AMV     6M'  OO  6W'   `Wb MM    MM    MM     8)   MM  MM    MM  
+ 8M    MM   MM    MM    MM    AMV      8M       8M     M8 MM    MM    MM      ,pm9MM  MM    MM  
+ YA.   MM   MM    MM    MM   AMV  , ,, YM.    , YA.   ,A9 MM    MM    MM  ,, 8M   MM  MM    MM  
+  `MbmdMM .JMML  JMML  JMML.AMMmmmM db  YMbmd'   `Ybmd9'.JMML  JMML  JMML.db `Moo9^Yo.`Mbod"YML.
+       MM                                                                                       
+     .JMML.                                                                                     
+       
+ * This code snippet is copyright at qmz.com.au
+ * - qmz.com.au is a top-ranking IT consulting firm that specialised in
+ * - Mobile Development, Web Development, Machine Learning, and Cloud Computing.
+ * - We provide individual and company training for professional development.
+ * - Find out more information in our official site：
+ * - https://qmz.com.au                                                                   
+ */
+
+// Find the last position of a target number in a sorted array. Return -1 if target does not exist.
+
+// Example
+
+// Given[1, 2, 2, 4, 5, 5].
+
+// For target =2, return 2.
+
+// For target =5, return 5.
+
+// For target =6, return -1.
+
+public class Solution {
+  /**
+   * @param nums: An integer array sorted in ascending order
+   * @param target: An integer
+   * @return an integer
+   */
+    
+}
