9ff6d46e5dfeba0ab3b80fc3b2f1c9c6ce97cf98
Alex Jiang <pigflyjky@gmail.com>
2019-07-21 22:50:39 +1000

    [Algorithm](Search)✅: solution for reverse linked list

--- LinkedList/Java/ReverseLinkedList.java
+++ LinkedList/Java/ReverseLinkedList.java
@@ -36,6 +36,16 @@
  */
 class Solution {
   public ListNode reverseList(ListNode head) {
-    
+    ListNode prev = null;
+    ListNode cur = head;
+
+    while (cur != null) {
+      ListNode temp = cur.next;
+      cur.next = prev;
+      prev = cur;
+      cur = temp;
+    }
+
+    return prev;
   }
 }
