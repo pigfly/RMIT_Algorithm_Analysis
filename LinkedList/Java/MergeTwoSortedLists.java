b5b08e7775428d533c6a8599e7df29c20bcd755e
Alex Jiang <pigflyjky@gmail.com>
2019-07-25 15:47:04 +1000

    [Algorithm](Linked List)✅: solution for merge two sorted lists

--- LinkedList/Java/MergeTwoSortedLists.java
+++ LinkedList/Java/MergeTwoSortedLists.java
@@ -33,6 +33,31 @@
  */
 class Solution {
   public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-      
+     if (l1 == null) { return l2; } 
+     if (l2 == null) { return l1; }
+
+     ListNode c1 = l1, c2 = l2;
+     ListNode dummy = new ListNode(0);
+     ListNode current = dummy;
+
+     while (c1 != null && c2 != null) {
+      if (c1.val < c2.val) {
+        current.next = c1;
+        c1 = c1.next;
+      } else {
+        current.next = c2;
+        c2 = c2.next;
+      }
+      current = current.next;
+     }
+
+     if (c1 == null) {
+       current.next = c2;
+     }
+     if (c2 == null) {
+       current.next = c1;
+     }
+
+     return dummy.next;
   }
 }
