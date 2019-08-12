ec1b1d7bfa168473e6425f9ac3ac7a2a1a0265a0
Alex Jiang <pigflyjky@gmail.com>
2019-08-12 16:43:42 +1000

    [Algorithm](DFS): problem import subset

---
+++ Search/Java/PartitionEqualSubsetSum.java
@@ -0,0 +1,48 @@
+/**                                                                                 
+  ,dW"Yvd `7MMpMMMb.pMMMb.  M"""MMV     ,p6"bo   ,pW"Wq.`7MMpMMMb.pMMMb.      ,6"Yb.`7MM  `7MM  
+ ,W'   MM   MM    MM    MM  '  AMV     6M'  OO  6W'   `Wb MM    MM    MM     8)   MM  MM    MM  
+ 8M    MM   MM    MM    MM    AMV      8M       8M     M8 MM    MM    MM      ,pm9MM  MM    MM  
+ YA.   MM   MM    MM    MM   AMV  , ,, YM.    , YA.   ,A9 MM    MM    MM  ,, 8M   MM  MM    MM  
+  `MbmdMM .JMML  JMML  JMML.AMMmmmM db  YMbmd'   `Ybmd9'.JMML  JMML  JMML.db `Moo9^Yo.`Mbod"YML.
+       MM                                                                                       
+     .JMML.                                                                                     
+       
+ * This code snippet is copyright at qmz.com.au
+ * - qmz.com.au is a top-ranking IT consulting firm that specialised in
+ * - Mobile Development, Web Development, Machine Learning, and Cloud Computing.
+ * - We provide individual and company training for professional development.
+ * - Find out more information in our official site：
+ * - https://qmz.com.au                                                                   
+ */
+
+// Given a non-empty array containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+
+// Note:
+
+// Each of the array element will not exceed 100.
+// The array size will not exceed 200.
+ 
+
+// Example 1:
+
+// Input: [1, 5, 11, 5]
+
+// Output: true
+
+// Explanation: The array can be partitioned as [1, 5, 5] and [11].
+ 
+
+// Example 2:
+
+// Input: [1, 2, 3, 5]
+
+// Output: false
+
+// Explanation: The array cannot be partitioned into equal sum subsets.
+
+class Solution {
+    public boolean canPartition(int[] nums) {
+        
+    }
+}
+  
