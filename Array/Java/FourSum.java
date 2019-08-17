7fd90d5c93dbde637a37fe37966192b4f2c5894a
Alex Jiang <pigflyjky@gmail.com>
2019-08-17 19:54:49 +1000

    [Algorithm](array): problem import four sum

---
+++ Array/Java/FourSum.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**                                                                                 
+  ,dW"Yvd `7MMpMMMb.pMMMb.  M"""MMV     ,p6"bo   ,pW"Wq.`7MMpMMMb.pMMMb.      ,6"Yb.`7MM  `7MM  
+ ,W'   MM   MM    MM    MM  '  AMV     6M'  OO  6W'   `Wb MM    MM    MM     8)   MM  MM    MM  
+ 8M    MM   MM    MM    MM    AMV      8M       8M     M8 MM    MM    MM      ,pm9MM  MM    MM  
+ YA.   MM   MM    MM    MM   AMV  , ,, YM.    , YA.   ,A9 MM    MM    MM  ,, 8M   MM  MM    MM  
+  `MbmdMM .JMML  JMML  JMML.AMMmmmM db  YMbmd'   `Ybmd9'.JMML  JMML  JMML.db `Moo9^Yo.`Mbod"YML.
+       MM                                                                                       
+     .JMML.                                                                                     
+       
+ * This code snippet is copyright at qmz.com.au
+ * - qmz.com.au is a top-ranking IT consulting firm that specialised in
+ * - Mobile Development, Web Development, Machine Learning, and Cloud Computing.
+ * - We provide individual and company training for professional development.
+ * - Find out more information in our official site：
+ * - https://qmz.com.au                                                                   
+ */
+
+// Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+
+// Note:
+
+// The solution set must not contain duplicate quadruplets.
+
+// Example:
+
+// Given array nums = [1, 0, -1, 0, -2, 2], and target = 0.
+
+// A solution set is:
+// [
+//   [-1,  0, 0, 1],
+//   [-2, -1, 1, 2],
+//   [-2,  0, 0, 2]
+// ]
+
+
+class Solution {
+  public List<List<Integer>> fourSum(int[] nums, int target) {
+      
+  }
+}
